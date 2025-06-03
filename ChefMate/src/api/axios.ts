3e61d2b4abb4620301acba5e71540ac831d161c3
Sue-Holding <sue.holding55@gmail.com>
2025-06-03 17:07:35 +0200

    fixed baseURL to have /api

--- ChefMate/src/api/axios.ts
+++ ChefMate/src/api/axios.ts
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:5000/api/', // or use ENV
-//   withCredentials: false // true if you use sessions/cookies
-// });
-
-// export default api;
-
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
@@ -20,3 +13,10 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api/', // or use ENV
+//   withCredentials: false // true if you use sessions/cookies
+// });
+
+// export default api;
