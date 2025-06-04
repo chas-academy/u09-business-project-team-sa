96789dba75993ba1684d3c723e44b049bdf6ce95
alex.ohlin96@gmail.com <youremail@example.com>
2025-06-04 18:10:52 +0200

    fix

--- backend-chefmate/src/services/googleService.ts
+++ backend-chefmate/src/services/googleService.ts
@@ -5,9 +5,7 @@ import session from "express-session";
 import cors from "cors";
 import { Mongoose } from "mongoose";
 
-const clientID =
-  "805405718661-8tba2pt2ham5sgfv4q8omvehddc9ujdd.apps.googleusercontent.com";
-const clientSecret = "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";
+
 
 passport.use(
   new GoogleStrategy(
