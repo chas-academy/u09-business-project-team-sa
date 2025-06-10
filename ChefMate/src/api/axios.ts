f6fcbde179f4c1b1c2f43b421a0db9d236d11b53
Sue-Holding <sue.holding55@gmail.com>
2025-06-10 13:04:58 +0200

    test with console log for baseurl connection

--- ChefMate/src/api/axios.ts
+++ ChefMate/src/api/axios.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, // Load from environment
-  withCredentials: true // If you're using cookies/session-based auth
+  // withCredentials: true // If you're using cookies/session-based auth
 });
+console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
